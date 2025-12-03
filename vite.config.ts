e9dc6e2e448add9e2cc81f1349aa50cb80459ea3
SunjanaPurba <sunjanapurba1@gmail.com>
2025-12-03 21:32:47 +0600

    updated

--- vite.config.ts
+++ vite.config.ts
@@ -1,31 +1,15 @@
-// import path from "path"
-// import { reactRouter } from "@react-router/dev/vite";
-// import tailwindcss from "@tailwindcss/vite";
-// import { defineConfig } from "vite";
-// import tsconfigPaths from "vite-tsconfig-paths";
-
-// export default defineConfig({
-//   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
-//   resolve: {
-//     alias: {
-//       "~": path.resolve(__dirname, "./app"),
-//     },
-//   },
-// });
-
-import path from "path";
-import react from "@vitejs/plugin-react";
+import path from "path"
+import { reactRouter } from "@react-router/dev/vite";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [react(), tailwindcss(), tsconfigPaths()],
+  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "./app"),
     },
   },
 });
-
 
