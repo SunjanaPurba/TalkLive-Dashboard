c86c969196957ef74f283a4f21c0de2389bd10d7
SunjanaPurba <sunjanapurba1@gmail.com>
2025-12-03 21:27:46 +0600

    Remove react-router dev plugin, fix Vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,31 @@
-import path from "path"
-import { reactRouter } from "@react-router/dev/vite";
+// import path from "path"
+// import { reactRouter } from "@react-router/dev/vite";
+// import tailwindcss from "@tailwindcss/vite";
+// import { defineConfig } from "vite";
+// import tsconfigPaths from "vite-tsconfig-paths";
+
+// export default defineConfig({
+//   plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+//   resolve: {
+//     alias: {
+//       "~": path.resolve(__dirname, "./app"),
+//     },
+//   },
+// });
+
+import path from "path";
+import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
-  plugins: [tailwindcss(), reactRouter(), tsconfigPaths()],
+  plugins: [react(), tailwindcss(), tsconfigPaths()],
   resolve: {
     alias: {
       "~": path.resolve(__dirname, "./app"),
     },
   },
 });
+
+
